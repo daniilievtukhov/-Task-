8d459f8201b81a6c7c07050aed52267fc9d4786b
Daniil Ievtukhov <114232481+daniilievtukhov@users.noreply.github.com>
2022-10-31 21:29:50 +0200

    Add files via upload

---
+++ task22.1.js
@@ -0,0 +1,120 @@
+"use strict";
+
+function Figure(x1, y1, x2, y2) {
+  this.__x1 = x1;
+  this.__y1 = y1;
+  this.__x2 = x2;
+  this.__y2 = y2;
+}
+function Rectangle(x1, y1, x2, y2) {
+  this.__x1 = x1;
+  this.__y1 = y1;
+  this.__x2 = x2;
+  this.__y2 = y2;
+  this.result = this.__rectangle();
+  this.info = function () {
+    return (
+      "Координата точки А{" +
+      this.__x1 +
+      "," +
+      this.__y1 +
+      "}" +
+      ". \n" +
+      "Координата точки В{" +
+      this.__x1 +
+      "," +
+      this.__y2 +
+      "}.\n" +
+      "Координата точки C{" +
+      this.__x2 +
+      "," +
+      this.__y2 +
+      "}.\n" +
+      "Координата точки D{" +
+      this.__x2 +
+      "," +
+      this.__y1 +
+      "}.\n" +
+      this.result
+    );
+  };
+}
+Rectangle.prototype = Object.create(Figure.prototype);
+Rectangle.prototype.constructor = Rectangle;
+Rectangle.prototype.getNumbers = function () {
+  return this.__x1, this.__y1, this.__x2, this.__y2;
+};
+Rectangle.prototype.setNumbers = function (x1, y1, x2, y2) {
+  this.__x1 = x1;
+  this.__y1 = y1;
+  this.__x2 = x2;
+  this.__y2 = y2;
+};
+
+Rectangle.prototype.__rectangle = function () {
+  var ad, ab;
+  this.__x2 > this.__x1
+    ? (ad = (this.__x2 - this.__x1) / 2)
+    : (ad = (this.__x1 - this.__x2) / 2);
+  this.__y2 > this.__y1
+    ? (ab = (this.__y2 - this.__y1) / 2)
+    : (ab = (this.__y1 - this.__y2) / 2);
+  return "Центр має координати: Х=" + ad + " " + "Y=" + ab;
+};
+
+function Circle(x1, y1, radius, x_center) {
+  this.__x1 = x1;
+  this.__y1 = y1;
+  this.__radius = radius;
+  this.__x_center = x_center;
+  this.result2 = this.__circle();
+  this.info = function () {
+    return (
+      "Точка А, яка знаходиться на колі, має координати {" +
+      this.__x1 +
+      "," +
+      this.__y1 +
+      "}" +
+      ". \n" +
+      "Радіус кола:" +
+      this.__radius +
+      ".\n" +
+      this.result2 +
+      "}"
+    );
+  };
+}
+Circle.prototype = Object.create(Figure.prototype);
+Circle.prototype.constructor = Circle;
+Circle.prototype.getCenter = function () {
+  return this.__radius, this.__x_center;
+};
+
+Circle.prototype.setCenter = function (x1, y1, radius, x_center) {
+  this.__x1 = x1;
+  this.__y1 = y1;
+  this.__radius = radius;
+  this.__x_center = x_center;
+};
+Circle.prototype.__circle = function () {
+  var o;
+  if (
+    this.__x_center >= 0 &&
+    this.__x1 >= 0 &&
+    this.__y1 >= 0 &&
+    this.__radius >= 0
+  ) {
+    o = Math.sqrt(
+      Math.pow(this.__radius, 2) -
+        Math.pow(this.__x1 - this.__x_center, 2) +
+        this.__y1
+    );
+    return "Центр O має координати: Х=" + this.__x_center + " " + "Y=" + o;
+  } else {
+    return false;
+  }
+};
+
+var res = new Rectangle(1, 2, 12, 8);
+
+console.log(res.info());
---
+++ task22.2.js
@@ -0,0 +1,40 @@
+"use strict";
+
+function Square(a) {
+  this.__a = a;
+}
+Square.prototype.getSide = function () {
+  return this.__a;
+};
+
+Square.prototype.setSide = function (a) {
+  this.__a = a;
+};
+Square.prototype.per = function () {
+  return this.__a * 4;
+};
+
+function Cube(a) {
+  Square.call(this, a);
+}
+
+Cube.prototype = Object.create(Square.prototype);
+
+Cube.prototype.constructor = Cube;
+
+Cube.prototype.getSide = function () {
+  return this._a;
+};
+
+Cube.prototype.setSide = function (a) {
+  this._a = a;
+};
+
+Cube.prototype.per = function () {
+  Square.prototype.per.call(this);
+  return Math.pow(this.__a, 2) * 6;
+};
+var p = new Square(12);
+console.log(p.per());
+var p1 = new Cube(12);
+console.log(p1.per());
