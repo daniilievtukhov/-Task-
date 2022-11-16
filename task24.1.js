68994fe0e354da003bbb1658296be57a8b5f13f2
Daniil Ievtukhov <114232481+daniilievtukhov@users.noreply.github.com>
2022-11-16 19:23:57 +0200

    Add files via upload

--- task24.1.js
+++ task24.1.js
@@ -1,19 +1,9 @@
 "use strict";
 
 var str = "Login1 LOgin2 login3 loGin4";
-var arr = [
-  str.substr(0, 5).toLowerCase(),
-  str.substr(6, 6).toLowerCase(),
-  str.substr(13, 6).toLowerCase(),
-  str.substr(20, 6).toLowerCase(),
-];
-
-var str1 = "Login1 LOgin2 login3 loGin4";
-
-var arr1 = str1.toLowerCase().split(" ");
-for (var i = 0; i < arr.length; i++){
-    arr[i] = arr[i].substr(0,arr[i].length-1)
+var arr = str.toLowerCase().split(" ");
+for (var i = 0; i < arr.length; i++) {
+  arr[i] = arr[i].substr(0, arr[i].length - 1);
 }
 
-
-console.log(a);
+console.log(arr);
